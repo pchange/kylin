e75e28ec843feb36f35a38c444370078ec193fbd
qt10 <qt10@gdzhsng.com>
2022-11-24 19:08:41 +0800

    feat

--- webapp/app/js/model/hybridInstanceManager.js
+++ webapp/app/js/model/hybridInstanceManager.js
@@ -52,7 +52,7 @@ KylinApp.service('hybridInstanceManager', function($q, HybridInstanceService, Pr
       _this.loading = false;
     },
     function() {
-      defer.reject('Failed to load models');
+      defer.reject('加载模型失败');
     });
 
     return defer.promise;
